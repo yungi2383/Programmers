823ebf076ca6874be455d95b4b6bb1761e3ee3d0
yungi2383 <141321269+yungi2383@users.noreply.github.com>
2023-11-17 19:10:16 +0900

    [level 1] Title: 폰켓몬, Time: 0.25 ms, Memory: 33.8 MB -BaekjoonHub

--- 프로그래머스/1/1845. 폰켓몬/폰켓몬.js
+++ 프로그래머스/1/1845. 폰켓몬/폰켓몬.js
@@ -1,14 +1,8 @@
+
+
 function solution(nums) {
-    let array = nums.sort((a,b) => a-b)
-    let array2 = [];
-    for (i=0;i<nums.length;i++){
-        if(!array[i-1] || array[i]!== array[i-1]) {
-            array2.push(array[i])
-        }
-    }
-    if (array2.length < nums.length/2) {
-        return array2.length
-    } else {
-        return nums.length/2
-    }
+  const max = nums.length / 2;
+  const arr = [...new Set(nums)];
+
+  return arr.length > max ? max : arr.length
 }
